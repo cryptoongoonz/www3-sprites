183f03d9ec536c687dfe0fbc7051216a093323a0
Jesse Friedland <jesse.friedland@gmail.com>
2023-01-05 12:00:02 -0500

    Change mascot only to be just split

--- mascot-only-sprite-cp.sh
+++ mascot-only-sprite-cp.sh
@@ -1,9 +1,4 @@
 for i in {1..10000}
 do
-  if [ $((i%2)) -eq 0 ]
-  then
-    cp tengu.png mascot-only/${i}.png
-  else
-    cp split.png mascot-only/${i}.png
-  fi
+  cp split.png mascot-only/${i}.png
 done
