fbeb3b31df19fc7282b45c26c10bcba49851c7a7
Jesse Friedland <jesse.friedland@gmail.com>
2023-01-05 11:43:30 -0500

    Switch mascot to being 50% tengu, 50% split

--- mascot-only-sprite-cp.sh
+++ mascot-only-sprite-cp.sh
@@ -1,4 +1,9 @@
 for i in {1..10000}
 do
-	cp split.png mascot-only-sprites/${i}.png
+  if [ $((i%2)) -eq 0 ]
+  then
+    cp tengu.png mascot-only/${i}.png
+  else
+    cp split.png mascot-only/${i}.png
+  fi
 done
